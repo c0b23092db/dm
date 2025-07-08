9338ee524e61baf4c6f51ef66773b140a09994b0
c0b23092 <c0b23092db@edu.teu.ac.jp>
2025-07-09 03:36:45 +0900

    Version Up : 1.0.2 -> 1.0.3 - コードの効率化

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,36 @@ use std::path::PathBuf;
 use dirs::download_dir;
 use crossterm::event::{self,Event};
 
-fn main() -> ExitCode{
+fn main() -> ExitCode {
     let Some(downloads_path) = download_dir() else {
-        println!("Failed to get Downloads Directory");
+        eprintln!("Failed to get path of the Downloads Directory");
         return ExitCode::FAILURE;
     };
+
     let args: Vec<String> = args().collect();
 
     if args.len() == 1 {
-        move_files(downloads_path, 1);
-    } else {
-        let input = &args[1];
-
-        if input == "dir" || input == "ls" {
-            list_files(downloads_path);
-            return ExitCode::SUCCESS;
-        }
-        match input.parse::<i32>() {
-            Ok(num) => move_files(downloads_path, num),
-            Err(_) => {
-                eprintln!("Invalid input: Please enter a number or dir/ls");
-                return ExitCode::FAILURE;
-            }
-        }
+        move_files(downloads_path,1);
+        return ExitCode::SUCCESS;
     }
+    let input = &args[1];
+    if input == "help" {
+        println!("Usage: dm [sum | help | dir/ls]");
+        println!("    sum     : 移動するファイルの数");
+        println!("    help    : ヘルプを表示");
+        println!("    dir/ls  : 移動するファイルの一覧を表示");
+        return ExitCode::SUCCESS;
+    }
+    if input == "dir" || input == "ls" {
+        show_list_files(downloads_path);
+        return ExitCode::SUCCESS;
+    }
+    if let Ok(num) = input.parse::<i32>() {
+        move_files(downloads_path, num);
+        return ExitCode::SUCCESS;
+    }
+
+    println!("Invalid input : Please enter help, a number or dir/ls");
     return ExitCode::SUCCESS;
 }
 
@@ -46,19 +52,22 @@ fn get_vecfiles_modified(directory_path:PathBuf, reverse_modified:bool) -> Vec<P
     return files;
 }
 
-fn list_files(directory_path:PathBuf) {
+fn show_list_files(directory_path:PathBuf) {
     let files = get_vecfiles_modified(directory_path,true);
+    let width:usize = files.len().to_string().len();
     for (index,file) in files.iter().enumerate() {
-        println!("[{:>width$}] {}",index + 1,
-            file.file_name().unwrap().to_string_lossy(),
-            width = files.len().to_string().len());
+        match file.file_name() {
+            Some(name) =>   println!("[{:>width$}] {}",
+                            index + 1,name.to_string_lossy(),width = width),
+            None => eprintln!("[Warning] Failed - {:?}", file),
+        }
     }
     pause().expect("Failed to input Key");
 }
 
 fn move_files(directory_path: PathBuf, count: i32) {
     let files = get_vecfiles_modified(directory_path,0 <= count);
-    if files.len() == 0 {
+    if files.is_empty() {
         println!("移動するファイルがありません。");
         return;
     }
@@ -69,9 +78,9 @@ fn move_files(directory_path: PathBuf, count: i32) {
             return;
         }
     };
-    let move_count = if count == 0 { files.len() } else { count.abs() as usize };
+    let move_count = if count == 0 { files.len() } else { count.unsigned_abs() as usize };
     for file in files.into_iter().take(move_count) {
-        if let Err(e) = rename(&file, &current_dir.join(file.file_name().unwrap())) {
+        if let Err(e) = rename(&file, current_dir.join(file.file_name().unwrap())) {
             eprintln!("Failed to move {:?} : {}", file, e);
         }
     }
@@ -83,9 +92,8 @@ fn pause() -> Result<(), Error> {
     loop {
         if event::poll(std::time::Duration::from_millis(1000))? {
             if let Event::Key(_) = event::read()? {
-                break;
+                Ok(());
             }
         }
     }
-    Ok(())
 }
